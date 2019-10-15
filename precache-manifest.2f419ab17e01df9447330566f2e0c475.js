f84bbeb9bff5235f4f746f31eed05f85c7ac5d70
SilentNinjaWorks <silentninjaworks@gmail.com>
2019-10-15 11:24:39 -0700

    Updates

---
+++ precache-manifest.2f419ab17e01df9447330566f2e0c475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0404b843faf838d0a6c70344b4b4f32e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a0680b476849068c8c6",
+    "url": "/monsters-rolodex/static/css/main.edc2ab75.chunk.css"
+  },
+  {
+    "revision": "58e81621b944b883b006",
+    "url": "/monsters-rolodex/static/js/2.cb468d4e.chunk.js"
+  },
+  {
+    "revision": "5a0680b476849068c8c6",
+    "url": "/monsters-rolodex/static/js/main.c9b423e1.chunk.js"
+  },
+  {
+    "revision": "a37d59c8fed6d8f0d19d",
+    "url": "/monsters-rolodex/static/js/runtime-main.710b24d4.js"
+  }
+]);
