61e9339a895ca7fa307b72a9a2be2ad784cae870
SilentNinjaWorks <silentninjaworks@gmail.com>
2019-10-15 09:35:30 -0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    // const { monsters, searchField } = this.state; -- was writted as a short version of:
+    // const monsters = this.state.monsters;
+    //const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      // from div to div its JSX syntax
+      <div className="App">
+        <h1> Monsters Rolodex </h1> 
+        
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+      /* CardList monsters = {this.state.monsters} before filtering */
+
+      // <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value })} /> -- SearchBox variable
+
+    );
+  }
 }
+
+ 
+// Life Cycle methods = methods that get called at different stages when this component gets rendered
 
 export default App;
